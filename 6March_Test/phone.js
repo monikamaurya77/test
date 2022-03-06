a4003833ab586b7c0bbe4e0725bb2c60b08d2d93
monikamaurya77 <96534851+monikamaurya77@users.noreply.github.com>
2022-03-06 20:01:39 +0530

    Add files via upload

---
+++ 6March_Test/phone.js
@@ -0,0 +1,27 @@
+//alert("abcdS")
+function vivo(){
+      document.write("vivo Y21 (Midnight Blue, 64 GB)  (4 GB RAM)");
+      document.write("₹13,990");
+}
+
+function iphone(){
+    document.write("APPLE iPhone 12 Pro (Pacific Blue, 128 GB))");
+    document.write("₹1,09,900");
+}
+function mi(){
+    document.write("Xiaomi 11i 5G (Camo Green, 128 GB)  (6 GB RAM)");
+    document.write("₹24,999");
+}
+function oppo(){
+    document.write("OPPO Reno7 Pro 5G (Startrails Blue, 256 GB)  (12 GB RAM");
+    document.write("₹39,999");
+}
+function realme(){
+    document.write("realme 9 Pro 5G (Midnight Black, 128 GB)  (6 GB RAM)");
+    document.write("₹17,999");
+}
+function samsung(){
+    document.write("SAMSUNG Galaxy Note 20 Ultra 5G (Mystic Bronze, 256 GB)  (12 GB RAM))");
+    document.write("₹1,04,999");
+}
+
